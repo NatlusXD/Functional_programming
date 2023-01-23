d22098d4f35dc2a7ae04fe43f9479d9c2d3af9d7
Sultan Mendibay <sultanmendibayxd@gmail.com>
2023-01-23 09:20:57 +0600

    Create asd.py

---
+++ asd.py
@@ -0,0 +1,30 @@
+name = "Giorno"
+surname = "Giovanna"
+age = 15
+stand_name = "Golden Experience"
+
+print(name)
+print(surname)
+print(age)
+print(stand_name)
+
+user_name = input("type your name")
+print(f"Your name: {user_name}")
+
+number = input("type number")
+number = int(number)
+
+if number > 0:
+    print("number is greater than 0")
+elif number == 0:
+    print("number is equal to 0")
+else:
+    print("number is lesser than 0")
+
+#if you see this - you are... you are...
+
+"""
+coffee is overrated
+change my mind
+"""
+
